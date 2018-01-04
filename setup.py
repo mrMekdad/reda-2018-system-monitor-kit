5e1ed11dc3223824e5a0e502594581b7d81442a1
mrMekdad <mekdadr6@gmail.com>
2018-01-04 20:29:00 +0100

    system-monitor-kit: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2018-system-monitor-kit",
+    version="0.1.0",
+    description="Terminal-first monitor for processes, snapshots, and CSV reports.",
+    author="Red@",
+    packages=["system_monitor_kit"],
+    package_dir={"": "src"},
+)
